7c750140e98801e8bf1bd76b13fbd1c1d95285fc
codeskyblue <codeskyblue@gmail.com>
2015-07-03 16:38:56 +0800

    add error file

---
+++ error.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author:  hzsunshx
+# Created: 2015-07-03 14:55
+
+"""
+error class
+"""
+
+class MoaError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
