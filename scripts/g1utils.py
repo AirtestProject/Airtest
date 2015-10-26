8f5f669c48dd0882ed8f5ac790d5fa4be1d29197
gzmaruijie <gzmaruijie@corp.netease.com>
2015-10-26 16:04:41 +0800

    add get/set gday/sday func

--- scripts/g1utils.py
+++ scripts/g1utils.py
@@ -1,5 +1,6 @@
 import requests
 HOST, PORT = "192.168.10.191", 17800
+HOST, PORT = "192.168.10.104", 27030 
 USERNUM = 31996757
 
 def server_call(cmd):
@@ -11,7 +12,39 @@ def server_call(cmd):
         })
     return r.text
 
+#$at g1/get1 $id var
+def get_var(varname):
+    return server_call('mobile_app/role_cache->get_var("$id@89","%s")' % varname)
+
+#$at g1/set1 $id var
+def set_var(varname, value):
+    if isinstance(value, basestring):
+        server_call('mobile_app/role_cache->set_var("$id@89","%s","%s")' % (varname, value))
+    else:
+        server_call('mobile_app/role_cache->set_var("$id@89","%s",%d)' % (varname, value))
+
+    return get_var(varname)
+
+#$at g1/gday $id var
+def get_daycnt(varname):
+    return server_call('mobile_app/module/counter->get_counter("$id@89","day","%s")' % varname)
+
+#$at g1/sday $id var
+def set_daycnt(varname,value):
+    if isinstance(value, basestring):
+        server_call('mobile_app/module/counter->set_counter("$id@89","day","%s", "%s")' % (varname, value))
+    else:
+        server_call('mobile_app/module/counter->set_counter("$id@89","day","%s", %d)' % (varname, value))
+
+    return get_var(varname)
+
 
 if __name__ == '__main__':
     server_call("$at h")
     print server_call("at/G1/sm/main->get_sm_leaf(\"$id@89\")")
+
+    print get_var('_gzj_true_pos')
+    print set_var('_gzj_true_pos',0 )
+    print get_daycnt('im_private_task')
+    print set_daycnt('im_private_task', 0)
+
