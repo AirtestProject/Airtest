1eecd53825b0dfe0a074bfe94f78fc8061c4922c
gzliuxin <gzliuxin@corp.netease.com>
2016-07-01 10:57:56 +0800

    add gzmobile tokenid as default

--- moa/plugins/testlab/user_config.py
+++ moa/plugins/testlab/user_config.py
@@ -6,4 +6,5 @@ do not push to git
 
 # 在stf-web个人设置页的“Setting UI”——“密钥”——“访问令牌”生成的，需要自行纪录一下
 # 密钥和节点是一一对应的，用别人的密钥会导致连接不上设备
-STF_TOKEN_ID = '1234567'
+# 这里默认用的gzmobile的机器人账号，如果你用自己的账号登录请更新这个token_ide
+STF_TOKEN_ID = 'tb77c4cf79ce940a6aeccc3ae987f1eb23cc2450b28544ca39662a56a8f546333'
