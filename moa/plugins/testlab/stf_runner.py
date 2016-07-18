257b64dede91d532bc37b6f6657ce62e2d90426b
gzliuxin <gzliuxin@corp.netease.com>
2016-07-18 17:18:59 +0800

    do not remove adbkeyboard

--- moa/plugins/testlab/stf_runner.py
+++ moa/plugins/testlab/stf_runner.py
@@ -186,6 +186,7 @@ def run_on_all_devices():
         try:
             addr = join(sn)
             dev = Android(addr, init_display=False, minicap=False, minitouch=False, init_ime=False)
+            # turn screen red to find device
             print dev.shell(['am', 'start', '-a', 'jp.co.cyberagent.stf.ACTION_IDENTIFY'])
         except:
             traceback.print_exc()
