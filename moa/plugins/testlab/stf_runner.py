c267e1b916a184b1ffe158abc01aa836f2fde149
gzliuxin <gzliuxin@corp.netease.com>
2016-07-20 11:28:27 +0800

    move run_all to stf_cleaner

--- moa/plugins/testlab/stf_runner.py
+++ moa/plugins/testlab/stf_runner.py
@@ -178,22 +178,6 @@ def main():
     func(*sys.argv[2:])
 
 
-def run_on_all_devices():
-    import subprocess
-    import traceback
-    for sn in devices():
-        try:
-            print subprocess.check_output("adb -s %s shell input keyevent HOME" % sn, shell=True)
-##            addr = join(sn)
-##            dev = Android(addr, init_display=False, minicap=False, minitouch=False, init_ime=False)
-##            # turn screen red to find device
-##            print dev.shell(['am', 'start', '-a', 'jp.co.cyberagent.stf.ACTION_IDENTIFY'])
-        except:
-            traceback.print_exc()
-        finally:
-            cleanup(sn)
-
 
 if __name__ == '__main__':
-##    main()
-    run_on_all_devices()
+    main()
