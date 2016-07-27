ecc2714f5554029b4b3bd43a5af8c74260d118f6
李翔 <lxn3032@corp.netease.com>
2016-07-27 14:19:15 +0800

    hunter plugin scan device list to get valid devid

--- moa/plugins/hunter.py
+++ moa/plugins/hunter.py
@@ -7,10 +7,19 @@ import requests
 from moa.core.main import DEVICE, logwrap, platform, get_platform
 
 
+def _handle_api_request_err(r, data):
+    print '======================================================================\n'
+    print '*** hunter api request err. ***'
+    print 'r.status_code=', r.status_code
+    print 'data=', data
+    print 'message=', r.json()['message']
+    print '======================================================================\n'
+
+
 @logwrap
-@platform(on=['Android','IOS'])
+@platform(on=['Android', 'IOS'])
 def get_wlanip():
-    if get_platform() == 'IOS': # temporary: using hardcode ip address for ios device
+    if get_platform() == 'IOS':  # temporary: using hardcode ip address for ios device
         return "10.254.140.145"
     else:
         netcfg = DEVICE.adb.shell('netcfg')
@@ -22,16 +31,21 @@ def get_wlanip():
 
 
 @logwrap
-@platform(on=['Android','IOS'])
-def get_hunter_devid(process):
+@platform(on=['Android', 'IOS'])
+def get_hunter_devid(tokenid, process):
     wlanip = get_wlanip()
-    if not wlanip:
-        return None
-    else:    
-        return '{}_at_{}'.format(process, wlanip.replace('.', '-'))
+    if wlanip:
+        devs = get_devices(token, process=process, ip=wlanip)
+        for devid, dev in devs.items():
+            dev_ret = hunter_sendto(tokenid,
+                    {'lang': 'python', 'data': 'require("safaia.console").write("sys", "ok")', 'devid': devid},
+                    watch_type='sys',
+                    need_reply=True)
+            if dev_ret and dev_ret['data'] == 'ok':
+                return devid
+    return None
 
 
-@logwrap
 def hunter_sendto(tokenid, data, **kwargs):
     data.update(kwargs)
     r = requests.post('http://hunter.nie.netease.com/api/sendto_device', headers={'tokenid': tokenid}, data=data)
@@ -41,16 +55,22 @@ def hunter_sendto(tokenid, data, **kwargs):
         except:
             pass
     else:
-        print '======================================================================\n'
-        print 'r.status_code=', r.status_code
-        print 'data=', data
-        print 'message=', r.json()['message']
-        print '======================================================================\n'
+        _handle_api_request_err(r, data)
+    return None
+
+
+def get_devices(tokenid, **kwargs):
+    r = requests.get('http://hunter.nie.netease.com/api/devices', headers={'tokenid': tokenid}, params=kwargs)
+    if r.status_code == 200:
+        try:
+            return r.json()['devices']
+        except:
+            pass
+    else:
+        _handle_api_request_err(r, kwargs)
     return None
 
 
 if __name__ == '__main__':
     token = "eyJhbGciOiJIUzI1NiIsImV4cCI6MTY0MDE1NTE5NiwiaWF0IjoxNDY3MzU1MTk2fQ.eyJ1c2VybmFtZSI6Imd6bGl1eGluIn0.GaWGg0E8_snNm3o6Zdn4P_evqKXTgea_0pLwdpb9TWI"
-    devid = "mh_at_10-251-81-101"
-    data = {'devid': devid, 'data': '$at h'}
-    hunter_sendto(token, data)
+    devs = get_devices(token, process='mh', ip='10.251.80.238')
