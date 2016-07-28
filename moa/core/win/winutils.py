d6491802da813b6ad0d3437fa3c445424cfc3909
王建军 <wjjn3033@corp.netease.com>
2016-07-28 16:38:30 +0800

    ide的依赖函数更新(获取父窗口的子窗口列表)

--- moa/core/win/winutils.py
+++ moa/core/win/winutils.py
@@ -421,6 +421,52 @@ class WindowMgr:
             pos = (rect[0], rect[1])
         return pos
 
+    def get_childhwnd_list_by_hwnd(self, hwnd, child_hwnd_list, w_h):
+        # 传入当前的child_hwnd_list，去除当前已经不在hwnd内的child_hwnd
+        #     如果发现全都不在了，那么就使用w_h进行查找新的child_hwnd_list
+        #     如果发现w_h的也不在了，那么就按照w_h的比例相同的原则进行查找，还没有，就报错...
+
+        # 第一步:根据父窗口的hwnd，将子窗口的hwnd、信息[x,y,w,h]一一提取出来：
+        self.target_child_hwnd_dict = {}
+        try:
+            win32gui.EnumChildWindows(hwnd, self._find_child_hwnd_in_target_wnd, None)
+        except:  # 该父窗口没有子窗口，直接return [].
+            return []
+        # 第二步：找到子窗口hwnd是否存在，剔除不存在的子窗口，如果child_hwnd_list中仍然有子窗口存在，直接返回：
+        all_child_hwnd_list = self.target_child_hwnd_dict.keys()
+        for c_hwnd in child_hwnd_list:
+            if c_hwnd not in all_child_hwnd_list:
+                child_hwnd_list.remove(c_hwnd)
+        if child_hwnd_list:
+            return child_hwnd_list
+        # 第三步：发现child_hwnd_list已变成[]，根据w_h重新查找一遍：
+        for key in all_child_hwnd_list:
+            width, height = self.target_child_hwnd_dict[key][2], self.target_child_hwnd_dict[key][3]
+            if width == w_h[0] and height == w_h[1]:
+                child_hwnd_list.append(key)
+        if child_hwnd_list:
+            return child_hwnd_list
+        # 第四步：发现child_hwnd_list仍为[]，则根据w_h的宽高比重新查找一遍：
+        for key in all_child_hwnd_list:
+            width, height = self.target_child_hwnd_dict[key][2], self.target_child_hwnd_dict[key][3]
+            if (width / height - w_h[0] / w_h[1]) < 0.00001:
+                child_hwnd_list.append(key)
+        if child_hwnd_list:
+            return child_hwnd_list
+        else:
+            raise Exception("no child_hwnd found, cannot get the precise pos of record-area! (from winutils.py)")
+
+    def _find_child_hwnd_in_target_wnd(self, hwnd, lparam):
+        '''本函数用于将单个父窗口的所有子窗口存入self.wnd_rect_origin'''
+        rect = win32gui.GetWindowRect(hwnd)
+        if win32gui.IsWindowVisible(hwnd):
+            # 将目标父窗口中的子窗口hwnd和rect一一加入目标self.target_child_hwnd_dict：
+            # self.target_child_hwnd_dict[hwnd] = rect
+            # 将[x,y,w,h]存入child_hwnd_dict的value内：
+            rect_xywh = [rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1]]
+            # hwnd_data = {"title": title, "resolution": reso, "target_child_hwnd_list": rect_xywh}
+            self.target_child_hwnd_dict[hwnd] = rect_xywh
+            
     def find_window(self, class_name, window_name = None):
         """find a window by its class_name"""
         self._handle = win32gui.FindWindow(class_name, window_name)
@@ -439,7 +485,7 @@ class WindowMgr:
         return self._wnd_title_toget
 
     def find_all_hwnd(self):
-        """获取所有窗口的hwnd."""
+        """获取所有一级窗口的hwnd."""
         def _wnd_enum_callback(hwnd, test_str=None):
             self._all_hwnd_list.append(hwnd)
 
@@ -471,6 +517,77 @@ class WindowMgr:
         self._handle_list_found = []
         win32gui.EnumWindows(self._window_enum_callback, wildcard)
         return self._handle_list_found
+
+    # def find_window_wildcard(self, wildcard):
+    #     self._handle_found = None
+    #     # win32gui.EnumWindows(self._window_enum_callback, wildcard)
+    #     self.find_hwnd_from_all_hwnd_info(wildcard)
+    #     return self._handle_found
+
+    # def find_window_list(self, wildcard):
+    #     self._handle_list_found = []
+    #     # win32gui.EnumWindows(self._window_enum_callback, wildcard)
+    #     self.find_hwnd_from_all_hwnd_info(wildcard)
+    #     return self._handle_list_found
+
+    # def find_hwnd_from_all_hwnd_info(self, wildcard):
+    #     self.all_hwnd_info_list = self.get_all_hwnd_info()
+    #     for hwnd_info in self.all_hwnd_info_list:
+    #         window_title, hwnd = hwnd_info["title"], hwnd_info["hwnd"]
+    #         if window_title:
+    #             try:
+    #                 m = re.search(wildcard, window_title)
+    #             except:
+    #                 m = None
+    #             if m != None:
+    #                 self._handle_found = hwnd
+    #                 self._handle_list_found.append(hwnd)
+    #                 # print window_title.encode("utf8")
+
+    # def get_all_hwnd_info(self):
+    #     all_hwnd_info_list = []  # 用于存储所有的窗口信息dict的列表
+    #     all_parent_hwnd = self.get_main_windows()
+    #     for parent_hwnd in all_parent_hwnd:
+    #         rect = win32gui.GetWindowRect(parent_hwnd)
+    #         window_title = win32gui.GetWindowText(parent_hwnd)
+    #         window_title = window_title.decode("gbk")
+    #         info = {"rect": rect, "title": window_title, "hwnd": parent_hwnd, "is_parent": 1}
+    #         all_hwnd_info_list.append(info)
+
+    #         try:
+    #             win32gui.EnumChildWindows(parent_hwnd, self.find_child_hwnd_in_parent_wnd, None)
+    #         except:
+    #             pass
+    #     for i in all_hwnd_info_list:
+    #         if i["title"]:
+    #             # print i["hwnd"], i["title"].encode("utf8")
+    #             pass
+    #     return all_hwnd_info_list
+
+    # def get_main_windows(self):
+    #     '''
+    #         获取一级窗口: Returns windows in z-order (top first)
+    #     '''
+    #     user32 = windll.user32
+    #     wndList = []
+    #     top = user32.GetTopWindow(None)
+    #     if not top:
+    #         return wndList
+    #     wndList.append(top)
+    #     while True:
+    #         next = user32.GetWindow(wndList[-1], win32con.GW_HWNDNEXT)
+    #         if not next:
+    #             break
+    #         wndList.append(next)
+    #     return wndList
+
+    # def find_child_hwnd_in_parent_wnd(self, hwnd, lparam):
+    #     """遍历子窗口回调函数"""
+    #     rect = win32gui.GetWindowRect(hwnd)
+    #     window_title = win32gui.GetWindowText(hwnd)
+    #     window_title = window_title.decode("gbk")
+    #     info = {"rect": rect, "title": window_title, "hwnd": hwnd, "is_parent": 0}
+    #     all_hwnd_info_list.append(info)
 
     def set_foreground(self):
         """put the window in the foreground"""
