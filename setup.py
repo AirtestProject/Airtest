0d6947fa9deeece427c38e5b92f653979f93f88a
gzliuxin <gzliuxin@corp.netease.com>
2016-09-05 15:25:22 +0800

    rm plugins to airtest.ext

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='moa',
+    name='airtest',
     version='0.0.4',
     author='Netease Game',
     author_email='gzliuxin@corp.netease.com',
     description='automated test package',
     long_description='automated test package from Netease Game',
-    url='http://git-qa.gz.netease.com/gzliuxin/moa',
+    url='http://git-qa.gz.netease.com/gzliuxin/airtest',
     license='MIT',
     keywords=['automation', 'test', 'android', 'opencv'],
     packages=find_packages(exclude=['cover', 'examples', 'tests', 'dist']),
     package_data={
-        'android_deps': ["*.apk", "moa/core/libs", "moa/core/adb"],
+        'android_deps': ["*.apk", "airtest/core/libs", "airtest/core/adb"],
         'html_statics': ["moa/report"]
     },
     install_requires=[
