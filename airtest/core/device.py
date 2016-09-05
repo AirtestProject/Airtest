8001cd60b0d0fec2af59e3b7ba8bb013cb09c6fc
gzliuxin <gzliuxin@corp.netease.com>
2016-09-05 17:09:09 +0800

    add base class for device

---
+++ airtest/core/device.py
@@ -0,0 +1,40 @@
+#encoding=utf-8
+
+class Device(object):
+    """base class for test device"""
+    def __init__(self):
+        super(Device, self).__init__()
+
+    def shell(self):
+        raise NotImplementedError
+
+    def start_app(self):
+        raise NotImplementedError
+
+    def stop_app(self):
+        raise NotImplementedError
+
+    def clear_app(self):
+        raise NotImplementedError
+
+    def install_app(self):
+        raise NotImplementedError
+
+    def uninstall_app(self):
+        raise NotImplementedError
+
+    def snapshot(self):
+        raise NotImplementedError
+
+    def touch(self):
+        raise NotImplementedError
+        
+    def swipe(self):
+        raise NotImplementedError
+        
+    def keyevent(self):
+        raise NotImplementedError
+        
+    def text(self):
+        raise NotImplementedError
+        
