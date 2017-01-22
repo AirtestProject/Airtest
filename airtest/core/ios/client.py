3db09233948f6b7aebfaa6c7b925d510264a978d
gzzhengshenshen <gzzhengshenshen@corp.netease.com>
2017-01-22 20:35:57 +0800

    IOS截图增加通过hunter截图的方法，截图耗时从约1.2s降低到约0.5s

--- airtest/core/ios/client.py
+++ airtest/core/ios/client.py
@@ -13,6 +13,9 @@ from hunter_cli import Hunter
 import time
 import wda
 import airtest
+from cStringIO import StringIO
+import base64
+from PIL import Image
 
 
 class XYTransformer(object):
@@ -44,10 +47,10 @@ class IOS(Device):
         self.driver = wda.Client('http://localhost:%d' % (port))
         sessionId = self.driver.status()['sessionId']
         self.session = wda.Session('http://localhost:%d' % (port), sessionId)
-        self.get_display_info()
         self.process = process
         hunter_tokenid = devip_tokenid.get(devip)
         self.hunter = Hunter(hunter_tokenid, process, devip=devip)
+        self.get_display_info()
 
     def shell(self):
         raise NotImplementedError
@@ -55,25 +58,41 @@ class IOS(Device):
     def home(self):
         self.driver.home()
 
-    def snapshot(self, filename="tmp.png"):
+    def snapshot(self, filename="tmp.png", quick=True):
         """
         take snapshot
         filename: save screenshot to filename
         """
-        datas = utils.screenshot(self.udid)
-        if filename:
-            try:
-                open(filename, 'wb').write(datas)
-            except IOError:
-                raise MoaError("snapshot error, write file %s failed" % filename)
+        if quick:
+            ##从hunter获取的图片大小和方向需要进行处理
+            b64img = self.hunter.script('console.screenshot', watch='ret')
+            datas = base64.b64decode(b64img)
+            file_data = StringIO(datas)
+            image = Image.open(file_data)
+            # trans
+            method = getattr(Image, 'ROTATE_{}'.format(360 - self.size["rotation"]))
+            image = image.transpose(method)
+            filename = filename or "tmp.png"
+            image.save(filename)
+            with open(filename) as f:
+                datas = f.read()
+
+        else:
+            datas = utils.screenshot(self.udid)
+            if filename:
+                try:
+                    open(filename, 'wb').write(datas)
+                except IOError:
+                    raise MoaError("snapshot error, write file %s failed" % filename)
         # 输出cv2对象
         screen = aircv.string_2_img(datas)
+        self.hunter_sca = 1.0 * self.size["height"] / screen.shape[:2][0] if quick else 1
         return screen
 
     def touch(self, pos, duration=None, isWDA=False):
-
+        pos = (pos[0] * self.hunter_sca, pos[1] * self.hunter_sca)
         if isWDA:
-            self.session.tap(pos[0] / self.scal, pos[1] / self.scal)
+            self.session.tap(pos[0] / self.wda_sca, pos[1] / self.wda_sca)
         else:
             x, y = XYTransformer.up_2_ori(
                 pos,
@@ -85,8 +104,11 @@ class IOS(Device):
             touch_mod(x, y)
 
     def swipe(self, fpos, tpos, duration=0.5, step=2, isWDA=True):
+        fpos = (fpos[0] * self.hunter_sca, fpos[1] * self.hunter_sca)
+        tpos = (tpos[0] * self.hunter_sca, tpos[1] * self.hunter_sca)
         if isWDA:
-            self.session.swipe(fpos[0] / self.scal, fpos[1] / self.scal, tpos[0] / self.scal, tpos[1] / self.scal, step)
+            self.session.swipe(fpos[0] / self.wda_sca, fpos[1] / self.wda_sca,
+                               tpos[0] / self.wda_sca, tpos[1] / self.wda_sca, step)
 
         else:
             x1, y1 = XYTransformer.up_2_ori(
@@ -140,7 +162,8 @@ class IOS(Device):
 
     def get_display_info(self):
         self.size = self.getPhysicalDisplayInfo()
-        self.scal = min(self.size.values()) / min(self.session.window_size())
+        self.wda_sca = min(self.size.values()) / min(self.session.window_size())
+        self.hunter_sca = 1
         self.size["orientation"] = self.getDisplayOrientation()
         self.size["rotation"] = self.size["orientation"] * 90
         return self.size
@@ -149,8 +172,8 @@ class IOS(Device):
         """
         get size of screen, height must be bigger then width
         """
-
-        screen = self.snapshot()
+        # have to take snapshot without quick
+        screen = self.snapshot(filename="init.png", quick=False)
         h, w = screen.shape[:2]
         size = dict(
             width=w,
@@ -187,10 +210,15 @@ if __name__ == "__main__":
     # driver.start_app('com.netease.mhxyhtb')
     # driver.stop_app('com.netease.mhxyhtb')
     # driver.home()
-    driver.touch((726, 168), isWDA=False)
+    # driver.touch((701, 168), isWDA=False)
+    # driver.touch((726, 168), isWDA=False)
 
     # driver.touch("123")
 
     # driver.swipe((482, 753) ,(482, 670))
+
+
+
+
 
 
