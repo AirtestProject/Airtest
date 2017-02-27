1870b9f2bb780b33cb447ae337a990870c65b5a8
李翔 <lxn3032@corp.netease.com>
2017-02-27 15:18:13 +0800

    auto refresh display info after snapshot

--- airtest/core/ios/client.py
+++ airtest/core/ios/client.py
@@ -34,11 +34,11 @@ class IOS(Device):
         self.driver = wda.Client(device_url)
         self.session = self.driver.session()
         self._last_activated_session_name = None  # package or bundleId
-        self._last_snapshot_image = None
+        self._size = {}
+        self._wda_sca = 1
 
-        # init display info
-        # attention：there is 3 size,phy_size/pic_size/wda_size
-        self.refresh_display_info()
+        # take a snapshot to refresh display info
+        self.snapshot()
 
     def shell(self):
         raise NotImplementedError
@@ -67,16 +67,15 @@ class IOS(Device):
 
         # 输出cv2对象
         screen = aircv.string_2_img(data)
-        self._last_snapshot_image = screen
-        self.refresh_display_info()
+        self.refresh_display_info(screen)
         return screen
 
     def touch(self, pos, duration=None):
-        self.session.tap(pos[0] / self.wda_sca, pos[1] / self.wda_sca)
+        self.session.tap(pos[0] / self._wda_sca, pos[1] / self._wda_sca)
 
     def swipe(self, fpos, tpos, duration=0.5, step=4):
-        self.session.swipe(fpos[0] / self.wda_sca, fpos[1] / self.wda_sca,
-                           tpos[0] / self.wda_sca, tpos[1] / self.wda_sca, step)
+        self.session.swipe(fpos[0] / self._wda_sca, fpos[1] / self._wda_sca,
+                           tpos[0] / self._wda_sca, tpos[1] / self._wda_sca, step)
 
     def keyevent(self, key):
         key = key.upper()
@@ -134,32 +133,12 @@ class IOS(Device):
         # utils.install_file(upload_file_path)
         # self.clear_app(upload_file_path)
 
-    def uninstall_app(self, appid):
+    def uninstall_app(self, package):
         pass
-        # utils.uninstall_app(appid, self.udid)
-
-    def refresh_display_info(self):
-        """
-        Returns:
-        display info as <dict>
-          rotation: <int> in degrees
-          height, width: <int> logic pixels of screen output, not the touch panel
-        """
-        self.size = {}
-        self.size["orientation"] = self.getDisplayOrientation()
-        self.size["rotation"] = self.size["orientation"] * 90
-        self.size["height"], self.size["width"] = self.getPhysicalDisplayInfo()
-        self.wda_sca = 1.0 * min(self.size["height"], self.size["width"]) / min(self.session.window_size())
-        print self.size
-        return self.size
+        # utils.uninstall_app(package, self.udid)
 
     def getPhysicalDisplayInfo(self):
-        """
-        get size of screen, height must be bigger then width
-        """
-        screen = self._last_snapshot_image or self._snapshot(filename="init_phy.png")
-        h, w = screen.shape[:2]
-        return h, w
+        pass
 
     def getDisplayOrientation(self):
         """
@@ -170,18 +149,28 @@ class IOS(Device):
 
     # use to resize
     def getCurrentScreenResolution(self):
-        w, h = self.size["width"], self.size["height"]
-        return w, h
+        return self._size["width"], self._size["height"]
 
     def refreshOrientationInfo(self, ori=None):
         """
         update dev orientation
         if ori is assigned, set to it(useful when running a orientation monitor outside)
         """
-        if ori is None:
-            ori = self.getDisplayOrientation()
-        self.size["orientation"] = ori
-        self.size["rotation"] = ori * 90
+        pass
+
+    def refresh_display_info(self, screen):
+        """
+        Returns:
+        display info as <dict>
+          rotation: <int> in degrees
+          height, width: <int> logic pixels of screen output, not the touch panel
+        """
+        self._size["orientation"] = self.getDisplayOrientation()
+        self._size["rotation"] = self._size["orientation"] * 90
+        self._size["height"], self._size["width"] = screen.shape[:2]
+        self._wda_sca = 1.0 * min(self._size["height"], self._size["width"]) / min(self.session.window_size())
+        print self._size
+        return self._size
 
     def get_device_external_ip(self):
         return self.driver.status()['ios']['ip']
