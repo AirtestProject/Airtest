b91930b699299301a02ab3239160e013a13071ca
刘欣 <gzliuxin@corp.netease.com>
2017-08-08 12:10:58 +0800

    add test_blackjack

---
+++ tests/test_blackjack.owl/test_blackjack.py
@@ -0,0 +1,22 @@
+#encoding=utf8
+
+__author__ = "刘欣"
+
+import os
+PKG = "org.cocos2d.blackjack"
+APK = os.path.join(ST.BASE_DIR, "blackjack-release-signed.apk")
+install(APK)
+amstop(PKG)
+wake()
+amstart(PKG)
+sleep(2)
+touch(MoaPic(r"tpl1499240443959.png", record_pos=(0.22, -0.165), resolution=(2560, 1536)))
+
+assert_exists(MoaPic(r"tpl1499240472304.png", "请下注", record_pos=(0.0, -0.094), resolution=(2560, 1536)))
+
+
+p = wait(MoaPic(r"tpl1499240490986.png", record_pos=(-0.443, -0.273), resolution=(2560, 1536)))
+
+touch(p)
+sleep(2)
+amstop(PKG)
