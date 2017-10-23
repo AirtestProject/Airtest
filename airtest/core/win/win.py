74e9bccb9d903bf78e4e7c86ef3976010f869c66
gzliuxin <gzliuxin@corp.netease.com>
2017-10-23 20:51:27 +0800

    use pywinauto in win; add test_win

--- airtest/core/win/win.py
+++ airtest/core/win/win.py
@@ -3,121 +3,101 @@ from airtest import aircv
 from airtest.core.device import Device
 from pywinauto.application import Application
 from pywinauto import Desktop
-from pywinauto.win32functions import SetForegroundWindow, ShowWindow
+from pywinauto.win32functions import SetForegroundWindow, ShowWindow, MoveWindow
 from pywinauto import mouse, keyboard
+from functools import wraps
+from .screen import screenshot
+import time
 import subprocess
+
+
+def require_app(func):
+    @wraps(func)
+    def wrapper(inst, *args, **kwargs):
+        if not inst.app:
+            raise RuntimeError("Connect to an application first to use %s" % func.__name__)
+        return func(inst, *args, **kwargs)
+    return wrapper
 
 
 class Windows(Device):
     """Windows client."""
 
     def __init__(self, **kwargs):
-        if not kwargs:
-            self.app = Desktop()
-            self.handle = None
-        else:
-            self.app = Application().connect(handle=handle, **kwargs)
-            self.handle = self.app.handle
+        self._app = Application()
+        self.app = None
+        self.mouse = mouse
+        self.keyboard = keyboard
+
+        if kwargs:
+            self.connect(**kwargs)
+
+    def connect(self, **kwargs):
+        self.app = self._app.connect(handle=handle, **kwargs)
 
     def shell(self, cmd):
         return subprocess.check_output(cmd, shell=True)
 
-    def snapshot(self, filename=None):
-        """default not write into file."""
-        # snapshot in window handle
-        if self.handle is not None:
-            return self.snapshot_by_hwnd(filename, self.handle)
-        # snapshot in full screen
-        screen = get_screen_shot()
-        if filename:
-            aircv.imwrite(filename, screen)
-        return screen
+    def snapshot(self, filename="tmp.png"):
+        if self.app:
+            screenshot(filename, self.app.handle)
+        else:
+            screenshot(filename)
+        return aircv.imread(filename)
 
     def keyevent(self, keyname, **kwargs):
-        pass
+        """https://pywinauto.readthedocs.io/en/latest/code/pywinauto.keyboard.html"""
+        self.keyboard.SendKeys(keyname)
 
     def text(self, text, **kwargs):
-        pass
+        self.keyevent(text)
 
     def touch(self, pos, **kwargs):
-        pass
+        """https://pywinauto.readthedocs.io/en/latest/code/pywinauto.mouse.html"""
+        self.mouse.click(coords=pos, **kwargs)
 
     def swipe(self, p1, p2, duration=0.8, steps=5):
-        pass
+        from_x, from_y = p1
+        to_x, to_y = p2
 
-    def set_foreground(self):
-        """将对应的窗口置到最前."""
-        pass
-
-    def get_window_pos(self):
-        pass
-
-    def set_window_pos(self, tuple_xy):
-        pass
+        interval = float(duration) / (steps + 1)
+        self.mouse.press(coords=(from_x, from_y))
+        time.sleep(interval)
+        for i in range(1, steps):
+            self.mouse.move(coords=(
+                from_x + (to_x - from_x) * i / steps,
+                from_y + (to_y - from_y) * i / steps,
+            ))
+            time.sleep(interval)
+        for i in range(10):
+            self.mouse.move(coords=(to_x, to_y))
+        time.sleep(interval)
+        self.mouse.release(coords=(to_x, to_y))
 
     def start_app(self, path):
-        return subprocess.call('start "" "%s"' % path, shell=True)
+        self.app = self._app.start(path)
 
-    def stop_app(self, title=None, pid=None, image=None):
-        if title:
-            cmd = 'taskkill /FI "WINDOWTITLE eq %s"' % title
-        elif pid:
-            cmd = 'taskkill /PID %s' % pid
-        elif image:
-            cmd = 'taskkill /IM %s' % image
-        return subprocess.check_output(cmd, shell=True)
+    def stop_app(self, pid):
+        self._app.connect(process=pid).kill()
 
-    def snapshot(self, filename="tmp.png"):
-        rect = win32gui.GetWindowRect(hwnd)
-        # pos = (rect[0], rect[1])
-        width = abs(rect[2] - rect[0])
-        height = abs(rect[3] - rect[1])
-        # print "in winutils.py WindowMgr():", pos, width, height
-        hwndDC = win32gui.GetWindowDC(hwnd)
-        mfcDC = win32ui.CreateDCFromHandle(hwndDC)
-        saveDC = mfcDC.CreateCompatibleDC()
+    @require_app
+    def set_foreground(self):
+        """将对应的窗口置到最前."""
+        SetForegroundWindow(self.app.wrapper_object())
 
-        saveBitMap = win32ui.CreateBitmap()
-        saveBitMap.CreateCompatibleBitmap(mfcDC, width, height)
-        saveDC.SelectObject(saveBitMap)
-        saveDC.BitBlt((0, 0), (width, height), mfcDC, (0, 0), win32con.SRCCOPY)
-        saveBitMap.SaveBitmapFile(saveDC, filename)
-        img = cv2.imread(filename)
-        return img
+    @require_app
+    def get_rect(self):
+        return self.app.wrapper_object().rectangle()
 
+    @require_app
+    def get_pos(self):
+        rect = self.get_rect()
+        return (rect.left, rect.top)
 
-def get_screen_shot(output="screenshot.png"):
-    hwnd = win32gui.GetDesktopWindow()
-    # print hwnd
-    # you can use this to capture only a specific window
-    #l, t, r, b = win32gui.GetWindowRect(hwnd)
-    #w = r - l
-    #h = b - t
+    @require_app
+    def move(self, tuple_xy):
+        self.app.MoveWindow(x=tuple_xy[0], y=tuple_xy[1])
 
-    # get complete virtual screen including all monitors
-    SM_XVIRTUALSCREEN = 76
-    SM_YVIRTUALSCREEN = 77
-    SM_CXVIRTUALSCREEN = 78
-    SM_CYVIRTUALSCREEN = 79
-    w = vscreenwidth = win32api.GetSystemMetrics(SM_CXVIRTUALSCREEN)
-    h = vscreenheigth = win32api.GetSystemMetrics(SM_CYVIRTUALSCREEN)
-    l = vscreenx = win32api.GetSystemMetrics(SM_XVIRTUALSCREEN)
-    t = vscreeny = win32api.GetSystemMetrics(SM_YVIRTUALSCREEN)
-    r = l + w
-    b = t + h
-    # print l, t, r, b, ' -> ', w, h
-
-    hwndDC = win32gui.GetWindowDC(hwnd)
-    mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
-    saveDC = mfcDC.CreateCompatibleDC()
-
-    saveBitMap = win32ui.CreateBitmap()
-    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
-    saveDC.SelectObject(saveBitMap)
-    saveDC.BitBlt((0, 0), (w, h),  mfcDC,  (l, t),  win32con.SRCCOPY)
-    saveBitMap.SaveBitmapFile(saveDC,  "screencapture.bmp")
-    # get screencapture in cv2 format
-    img = cv2.imread("screencapture.bmp")
-    # delete temp file:
-    os.remove("screencapture.bmp")
-    return img
+    @require_app
+    def kill(self, pid):
+        self.app.kill()
--- tests/test_win.py
+++ tests/test_win.py
@@ -1,220 +1,36 @@
 # encoding=utf-8
-import sys
-sys.path.append("..\\..\\..\\")
-
 from airtest.core.win import Windows
-from airtest.core.win.winsendkey import handle_code, parse_keys, KeyAction
-from airtest.core.win.mouse import get_mouse_point, mouse_dclick, mouse_click, mouse_down, mouse_up
+import subprocess
 import unittest
-import mock
-from mock import patch,Mock
-import win32gui
 import numpy
-import time
 
-
-try:
-    str_class = basestring
-
-    def enforce_unicode(text):
-        return unicode(text)
-except NameError:
-    str_class = str
-
-    def enforce_unicode(text):
-        return text
-
-
-# should move in class
-window = []
-def handcallback(hwnd,exc):
-    window.append(hwnd)
 
 class TestWin(unittest.TestCase):   
 
     @classmethod
     def setUpClass(cls):
-        case=""
-        cls.window=Windows()
-        win32gui.EnumWindows(handcallback,"")
+        cls.windows = Windows()
 
-    @classmethod
-    def tearDownClass(self):
-        self.window.stop_app(image="calc.exe")
-
-                
     def test_shell(self):
-        #print window
-        result=self.window.shell("dir")
-        print(type(result))
-        self.assertIsInstance(result,str_class)
-
+        result=self.windows.shell("dir")
+        self.assertIn(".", result)
+        self.assertIn("..", result)
 
     def test_snapshot(self):
-        with mock.patch.object(self.window,'handle',new=window[2]):
-            with mock.patch.object(self.window,'snapshot_by_hwnd',return_value="666"):
-                result=self.window.snapshot() 
-                self.assertEqual(result,"666")
-                self.assertEqual(self.window.snapshot_by_hwnd.called,True)
+        result = self.windows.snapshot(filename="win_snapshot.png") 
+        self.assertIsInstance(result, numpy.ndarray)
 
-
-        result=self.window.snapshot(filename="win_snapshot.png") 
-        self.assertIsInstance(result,numpy.ndarray)
-
-    def test_snapshot_by_hwnd(self):
-        with self.assertRaises(Exception):
-            self.window.snapshot_by_hwnd(hwnd_to_snap=0)
-
-        # try both method
-        result1=self.window.snapshot_by_hwnd(hwnd_to_snap=window[2],use_crop_screen=True)
-        self.assertIsInstance(result1,numpy.ndarray)
-        result2=self.window.snapshot_by_hwnd(hwnd_to_snap=window[2],use_crop_screen=False)
-        self.assertIsInstance(result2,numpy.ndarray)
-
-    def test_get_wnd_pos_by_hwnd(self):
-        all_hwnd_list = self.window.find_all_hwnd()
-        hwnd_to_snap = all_hwnd_list[0]
-        wnd_pos = self.window.get_wnd_pos_by_hwnd(hwnd_to_snap)
-        self.assertIsInstance(wnd_pos, tuple)
-
-
-
-
-    def test_get_childhwnd_list_by_hwnd(self):
-        hwnd_list = self.window.get_childhwnd_list_by_hwnd(0, [], (100, 100))
-        self.assertIsInstance(hwnd_list, list)
-
-    '''
-        start test input things
-    '''
     def test_keyevent(self):
-        self.window.start_app("calc")
-        time.sleep(1)
-        self.window.find_window("计算器")
-        self.window.keyevent("1")
-        # 输入特殊字符，走入特定逻辑
-        self.window.keyevent("A", escape=True)
-        self.window.keyevent(" ")
-        self.window.keyevent("~")
-        self.window.keyevent("A")
-        self.window.keyevent("a", combine="ctrl", escape=True)
-
+        self.windows.keyevent("abc{ENTER}")
 
     def test_text(self):
-        self.window.start_app("calc")
-        time.sleep(1)
-        self.window.find_window("计算器")
-        self.window.text("12")
-        self.window.text("(123)")
-
+        self.windows.text("abc")
 
     def test_touch(self):
-        self.window.touch((0,0),True)
-        self.window.touch((0,0),False)
+        self.windows.touch((11, 11))
 
     def test_swipe(self):
-        self.window.swipe((0,0),(10,10))
-
-    '''
-        find window stuff
-    '''
-    def test_find_window(self):
-        result=self.window.find_window(u"开始")
-        self.assertIsInstance(result,int)
-    def test_find_hwnd_title(self):
-        result=self.window.find_hwnd_title(window[1])
-        self.assertIsInstance(result,unicode)
-
-    def test_find_all_hwnd(self):
-        result=self.window.find_all_hwnd()
-        self.assertIsInstance(result,list)
-
-    #todo find_window_list
-
-
-
-
-    def test_getCurrentScreenResolution(self):
-        resolution = self.window.getCurrentScreenResolution()
-        self.assertIsInstance(resolution, tuple)
-
-
-    # mock handle
-    def test_set_foreground(self):
-        with mock.patch.object(self.window,'handle',new=window[2]):
-            with mock.patch.object(self.window.winmgr,'handle',new=window[2]):
-                self.window.set_foreground()
-
-    def test_get_window_pos(self):
-        with mock.patch.object(self.window.winmgr,'handle',new=window[2]):
-            result=self.window.get_window_pos()
-            self.assertIsNotNone(result)
-
-    def test_operate(self):
-        with self.assertRaises(Exception):
-            self.window.oprate({"type": "strange"})
-        self.window.operate({"type": "down", "x": 10, "y": 10})
-        self.window.operate({"type": "move", "x": 10, "y": 10})
-        self.window.operate({"type": "up", "x": 10, "y": 10})
-
-    # test start process
-    def test_start_app(self):
-        ret = self.window.start_app("calc")
-        self.assertEqual(ret, 0)
-
-    def test_stop_app(self):
-        # self.windows.stop_app(title="计算器")
-        self.window.stop_app(image="calc.exe")
-
-
-class TestSendKey(unittest.TestCase):
-
-    def test_handle_code(self):
-        code_keys = handle_code("{(asd)asd}")
-        self.assertIsInstance(code_keys, list)
-
-    def test_parse_keys(self):
-        code_keys = parse_keys("{(asd)asd+-}")
-        self.assertIsInstance(code_keys, list)
-
-
-class TestMouse(unittest.TestCase):
-
-    def test_get_mouse_point(self):
-        mouse_point = get_mouse_point()
-        self.assertIsInstance(mouse_point, tuple)
-
-    def test_mouse_dclick(self):
-        mouse_dclick(x=0, y=0)
-
-    def test_mouse_click(self):
-        mouse_click([10, 0], right_click=True)
-        mouse_click([0, 0], shift=True, duration=0.05)
-
-        mouse_dclick(x=0, y=0)
-
-
-    def test_mouse_down_up(self):
-        mouse_down(pos=[2, 0], right_click=True)
-        mouse_up(right_click=True)
-        mouse_down(pos=[0, 0])
-        mouse_up()
-
-        # close right click
-        mouse_dclick(x=0, y=0)
-
-
-###############################################
-
-
-
-
-
-
-
-# todo be better
-#class kkk():
-#numpy.ndarray
+        self.windows.swipe((11, 11), (100, 100))
 
 
 if __name__ == '__main__':
