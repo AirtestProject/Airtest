8ce6d113cd9049e6a027af770edeef5c24d82ebe
gzliuxin <gzliuxin@corp.netease.com>
2017-12-07 16:18:20 +0800

    fix xy.py as example launcher

--- playground/xy.py
+++ playground/xy.py
@@ -5,12 +5,13 @@ from airtest.cli.parser import runner_parser
 class G18AirtestCase(AirtestCase):
 
     def setUp(self):
-        self.scope["hunter"] = "i am hunter"
-
-        self.exec_other_script("pre.owl")
+        # self.scope["hunter"] = "i am hunter"
+        # self.exec_other_script("pre.owl")
+        print("custom setup")
 
     def tearDown(self):
-        self.exec_other_script("post.owl")
+        # self.exec_other_script("post.owl")
+        print("custom tearDown")
 
 
 if __name__ == '__main__':
