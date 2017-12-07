4526a52754d965f775c9f77406894a9041234a3d
gzliuxin <gzliuxin@corp.netease.com>
2017-12-07 17:20:09 +0800

    add more comments in xy.py

--- playground/xy.py
+++ playground/xy.py
@@ -2,19 +2,24 @@ from airtest.cli.runner import AirtestCase, run_script
 from airtest.cli.parser import runner_parser
 
 
-class G18AirtestCase(AirtestCase):
+class CustomAirtestCase(AirtestCase):
 
     def setUp(self):
+        # add var/function/class/.. to globals
         # self.scope["hunter"] = "i am hunter"
-        # self.exec_other_script("pre.owl")
+        # self.scope["add"] = lambda x: x+1
+
+        # exec setup script
+        # self.exec_other_script("setup.owl")
         print("custom setup")
 
     def tearDown(self):
-        # self.exec_other_script("post.owl")
+        # exec teardown script
+        # self.exec_other_script("teardown.owl")
         print("custom tearDown")
 
 
 if __name__ == '__main__':
     ap = runner_parser()
     args = ap.parse_args()
-    run_script(args, G18AirtestCase)
+    run_script(args, CustomAirtestCase)
