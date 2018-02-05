c3c4834f94f869969561f01b03510e92c0971769
gzliuxin <gzliuxin@corp.netease.com>
2018-02-05 12:49:02 +0800

    cleanup forward without get proc output to prevent deadlock atexit

--- airtest/core/android/adb.py
+++ airtest/core/android/adb.py
@@ -699,8 +699,10 @@ class ADB(object):
         Returns:
             None
         """
-        for local in self._forward_local_using[:]:
-            self.remove_forward(local)
+        for local in self._forward_local_using:
+            self.start_cmd(["forward", "--remove", local])
+
+        self._forward_local_using = []
 
     @property
     def line_breaker(self):
