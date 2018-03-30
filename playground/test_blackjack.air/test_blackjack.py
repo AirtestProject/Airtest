a2922fe9f1804836706724f02a034607cc874c3d
cclauss <cclauss@bluewin.ch>
2018-03-30 07:14:33 +0200

    from airtest.core.api import the undefined names
    Should fix the flake8 tests.

--- playground/test_blackjack.air/test_blackjack.py
+++ playground/test_blackjack.air/test_blackjack.py
@@ -4,6 +4,8 @@ __author__ = "刘欣"
 
 import os
 from time import sleep
+from airtest.core.api import (Template, device, install,
+                              start_app, stop_app, touch, wake)
 
 PWD = args.script
 PKG = "org.cocos2d.blackjack"
@@ -17,7 +19,6 @@ sleep(2)
 touch(Template(r"tpl1499240443959.png", record_pos=(0.22, -0.165), resolution=(2560, 1536)))
 
 assert_exists(Template(r"tpl1499240472304.png", record_pos=(0.0, -0.094), resolution=(2560, 1536)), "请下注")
-
 
 p = wait(Template(r"tpl1499240490986.png", record_pos=(-0.443, -0.273), resolution=(2560, 1536)))
 
