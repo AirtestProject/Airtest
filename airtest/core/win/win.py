ffa3f444a8794f502de3c8521729312857aed0e9
王建军 <wjjn3033@corp.netease.com>
2018-04-10 19:04:15 +0800

    fix: 限定pywinauto版本，避免py2环境下触发import报错。

--- airtest/core/win/win.py
+++ airtest/core/win/win.py
@@ -2,7 +2,7 @@
 from airtest import aircv
 from airtest.core.device import Device
 from pywinauto.application import Application
-from pywinauto.win32functions import SetForegroundWindow   # ,SetProcessDPIAware
+from pywinauto.win32functions import SetForegroundWindow  # ,SetProcessDPIAware
 from pywinauto import mouse, keyboard
 from functools import wraps
 from .screen import screenshot
