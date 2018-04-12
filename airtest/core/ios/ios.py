0c08dc09d655cbff40236c371c92e0427fbe8a22
suyuchen <suyuchen@corp.netease.com>
2018-04-12 21:23:23 +0800

    调整touch功能大小缩放 和 截图方向，both work now

--- airtest/core/ios/ios.py
+++ airtest/core/ios/ios.py
@@ -16,7 +16,11 @@ else:
 from airtest import aircv
 from airtest.core.device import Device
 from airtest.core.ios.constant import CAP_METHOD, TOUCH_METHOD, IME_METHOD
+from airtest.core.ios.rotation import XYTransformer
 from airtest.utils.logger import get_logger
+
+# roatations of ios
+from wda import LANDSCAPE, PORTRAIT, LANDSCAPE_RIGHT, PORTRAIT_UPSIDEDOWN
 
 
 logger = get_logger(__name__)
@@ -45,17 +49,37 @@ class IOS(Device):
         # use to click/swipe/close app/get wda size
         wda.DEBUG = True
         self.driver = wda.Client(addr)
-        self._size = {'width': 0, 'height': 0}
+
+        # record device's width
+        self._size = {'width': None, 'height': None}
         self._touch_factor = 0.5
+        self._last_orientation = None
+
 
     @property
     def session(self):
         return self.driver.session()
 
+    def window_size(self):
+        """ 
+            return window size
+            namedtuple:
+                Size(wide , hight)
+        """
+        return self.driver.session().window_size()
+
+    @property
+    def orientation(self):
+        """
+            return device oritantation status
+            in  LANDSACPE POR
+        """
+        return self.driver.session().orientation
+
     def home(self):
         return self.driver.home()
 
-    def snapshot(self, filename=None):
+    def snapshot(self, filename=None, ensure_orientation=True):
         """
         take snapshot
         filename: save screenshot to filename
@@ -67,7 +91,7 @@ class IOS(Device):
         elif self.cap_method == CAP_METHOD.MINICAP_STREAM:
             raise NotImplementedError
         elif self.cap_method == CAP_METHOD.WDACAP:
-            data = self.driver.screenshot(filename)  # wda 截图不用考虑朝向
+            data = self.driver.screenshot()  # wda 截图不用考虑朝向
         
         # output cv2 object 
         try:
@@ -78,22 +102,63 @@ class IOS(Device):
             traceback.print_exc()
             return None
 
+        now_orientation = self.orientation
+
+        # ensure the orientation is right
+        if ensure_orientation and now_orientation in [LANDSCAPE, LANDSCAPE_RIGHT]:
+
+            # minicap screenshots are different for various sdk_version
+            if self.cap_method in (CAP_METHOD.MINICAP, CAP_METHOD.MINICAP_STREAM) and self.sdk_version <= 16:
+                h, w = screen.shape[:2]  # cvshape是高度在前面!!!!
+                if w < h:  # 当前是横屏，但是图片是竖的，则旋转，针对sdk<=16的机器
+                    screen = aircv.rotate(screen, self.display_info["orientation"] * 90, clockwise=False)
+
+            # wda 截图是要根据orientation旋转
+            elif self.cap_method == CAP_METHOD.WDACAP:
+                screen = aircv.rotate(screen, 90, clockwise= (now_orientation== LANDSCAPE_RIGHT) ) 
+      
+
+        # readed screen size
+        h, w = screen.shape[:2]
+
+        # save last res for portrait
+        if now_orientation in [LANDSCAPE, LANDSCAPE_RIGHT]:
+            self._size['height'] = w
+            self._size['width'] = h
+        else:
+            self._size['height'] = h
+            self._size['width'] = w
+
+        winw,winh = self.window_size()
+
+        self._touch_factor = float(winh) / float(h)
+
+        # save as file if needed
+        if filename:
+            aircv.imwrite(filename, screen)
+
         return screen
 
+
     def touch(self, pos, times=1, duration=0.01):
+        # trans pos of click
+        pos = self._touch_point_by_orientation(pos)
+
+        # scale touch postion 
         x, y = pos[0] * self._touch_factor, pos[1] * self._touch_factor
-        # if times == 2:
-        #     self.session.double_tap(x, y)
-        # else:
-        #     for _ in range(times):
-        #         self.session.tap(x, y)
-        r = requests.get(urljoin(self.addr, "status"))
-        sid = r.json()["sessionId"]
-        url = urljoin(self.addr, "session/%s/wda/tap/0" % sid)
-        print(url, x, y)
-        r = requests.post(url, json={"x": x, "y": y})
-        print(r.json())
-        return r
+        
+        if times == 2:
+            self.session.double_tap(x, y)
+        else:
+            for _ in range(times):
+                self.session.tap(x, y)
+        #r = requests.get(urljoin(self.addr, "status"))
+        #sid = r.json()["sessionId"]
+        #url = urljoin(self.addr, "session/%s/wda/tap/0" % sid)
+        #print(url, x, y)
+        #r = requests.post(url, json={"x": x, "y": y})
+        #print(r.json())
+        #return r
 
     def swipe(self, fpos, tpos, duration=0.5):
         self.session.swipe(fpos[0] * self._touch_factor, fpos[1] * self._touch_factor,
@@ -141,6 +206,42 @@ class IOS(Device):
         """
         return self.driver.status()
 
+    def _touch_point_by_orientation(self, tuple_xy):
+        """
+        Convert image coordinates to physical display coordinates, the arbitrary point (origin) is upper left corner
+        of the device physical display
+
+        Args:
+            tuple_xy: image coordinates (x, y)
+
+        Returns:
+
+        """
+        x, y = tuple_xy
+
+        # use correct w and h due to now orientation
+        # _size 只对应竖直时候长宽
+        now_orientation = self.orientation
+
+        if now_orientation in [PORTRAIT, PORTRAIT_UPSIDEDOWN]:
+            width, height =  self._size['width'], self._size["height"]
+        else:
+            height, width = self._size['width'], self._size["height"]
+
+        # check if not get screensize when touching
+        if not width or not height:
+            # use snapshot to get current resuluton
+            self.snapshot()
+
+        x, y = XYTransformer.up_2_ori(
+            (x, y),
+            (width, height),
+            now_orientation
+        )
+        return x, y
+
+    def _check_orientation_change(self):
+        pass
 
 if __name__ == "__main__":
     start = time.time()
