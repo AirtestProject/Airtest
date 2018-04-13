cda255d5329f3f751a8fbf5dca3493d0d4ee6dac
gzliuxin <gzliuxin@corp.netease.com>
2018-04-13 11:27:35 +0800

    remove pip.req in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import sys
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt', session=False)
-reqs = [str(ir.req) for ir in install_reqs]
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file. (replacing from pip.req import parse_requirements)"""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+reqs = parse_requirements('requirements.txt')
 if sys.platform == "win32":
     reqs.append('pywinauto==0.6.3')
     reqs.append('pywin32')
