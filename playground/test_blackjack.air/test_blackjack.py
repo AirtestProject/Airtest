ff8bc6f79783ff777a66f060c7b78c0f1eb66591
gzliuxin <gzliuxin@corp.netease.com>
2018-04-17 10:48:18 +0800

    fix adb.swipe & add swipe in test_blackjack

--- playground/test_blackjack.air/test_blackjack.py
+++ playground/test_blackjack.air/test_blackjack.py
@@ -25,6 +25,11 @@ assert_exists(Template(r"tpl1499240472304.png", record_pos=(0.0, -0.094), resolu
 p = wait(Template(r"tpl1499240490986.png", record_pos=(-0.443, -0.273), resolution=(2560, 1536)))
 
 touch(p)
+
+swipe(Template(r"tpl1523932626575.png", record_pos=(-0.266, 0.105), resolution=(1920, 1080)), vector=[0.0005, -0.4023])
+assert_exists(Template(r"tpl1523933150565.png", record_pos=(-0.213, 0.103), resolution=(1920, 1080)), "Swipe succeed")
+
+
 sleep(2)
 stop_app(PKG)
 
