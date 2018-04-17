edf903941afee29d74a957318e26058eae330732
gzliuxin <gzliuxin@corp.netease.com>
2018-04-17 14:26:14 +0800

    level up to 1.0.7 & update reademe

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.platform == "win32":
 
 setup(
     name='airtest',
-    version='1.0.6',
+    version='1.0.7',
     author='Netease Games',
     author_email='gzliuxin@corp.netease.com',
     description='UI Test Automation Framework for Games and Apps',
