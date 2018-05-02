1f40abd0b99b4a0a96114d510bd44d6e2853c9b6
祝啸风 <zxfn4514@corp.netease.com>
2018-05-02 10:51:18 +0800

    删除错误提交kivy目录

--- airtest/core/kivy/test.py
+++
@@ -1,9 +0,0 @@
-from agent import Agent
-from controler import Controler
-import aircv
-
-agent = Agent(dev_ip="10.254.46.45")
-controler = Controler(agent)
-
-controler.swipe((0,0), (550,0))
-print controler.get_display_info()
