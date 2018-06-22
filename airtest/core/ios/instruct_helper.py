70999143ef5d8be7248ef3b03ab27bf0cbc0cf59
suyuchen@corp.netease.com <suyuchen@corp.netease.com>
2018-06-22 17:29:03 +0800

    mod: helper for run instructions like iproxy

---
+++ airtest/core/ios/instruct_helper.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import traceback
+import time
+import sys
+from airtest.core.error import AirtestError
+from airtest.utils.snippet import reg_cleanup, on_method_ready, get_std_encoding
+from airtest.utils.logger import get_logger
+
+LOGGING = get_logger(__name__)
+
+
+class InstructHelper(object):
+    """
+    ForwardHelper class
+    or help run other Instruction
+    """
+
+    proxy_process = 'iproxy'
+
+    def __init__(self):
+        self.subprocessHandle = []
+        reg_cleanup(self.teardown)
+
+    @on_method_ready('start')
+    def get_ready(self):
+        pass
+
+    def teardown(self):
+        # stop all process started by self
+        for sub_proc in self.subprocessHandle:
+            sub_proc.kill()
+
+    def do_proxy(self, remote_port, local_port):
+        """
+        Start do proxy of ios device and self device
+
+        Returns:
+            None
+
+        """
+
+        assert(str(remote_port).isdigit())
+        assert(str(local_port).isdigit())
+
+        cmds = [self.proxy_process, str(remote_port), str(local_port)]
+
+        proc = subprocess.Popen(
+            cmds,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        # something like port binding fail
+        time.sleep(0.5)
+
+        if proc.poll() is not None:
+            stdout, stderr = proc.communicate()
+            stdout = stdout.decode(get_std_encoding(sys.stdout))
+            stderr = stderr.decode(get_std_encoding(sys.stderr))
+            raise AirtestError((stdout, stderr))
+
+        self.subprocessHandle.append(proc)
+
+if __name__ == '__main__':
+    ins = InstructHelper()
+    ins.do_proxy(8100, 8100)
