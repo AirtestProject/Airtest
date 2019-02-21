af17e42c34eae5d2f3cfc3d94a77fe83b8c887ca
王建军 <wjjn3033@corp.netease.com>
2019-02-21 20:59:20 +0800

    add: keypoint matching base class.

--- airtest/aircv/error.py
+++ airtest/aircv/error.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Declaration:
-    Define all BaseError Classes used in aircv.
-"""
+"""Declaration: Define all BaseError Classes used in aircv."""
 
 
 class BaseError(Exception):
@@ -14,34 +11,37 @@ class BaseError(Exception):
         self.message = message
 
     def __repr__(self):
+        """Output message."""
         return repr(self.message)
 
 
 class FileNotExistError(BaseError):
     """Image does not exist."""
-    pass
 
 
 class TemplateInputError(BaseError):
     """Resolution input is not right."""
-    pass
 
 
 class NoSIFTModuleError(BaseError):
     """Resolution input is not right."""
-    pass
 
 
 class NoSiftMatchPointError(BaseError):
     """Exception raised for errors 0 sift points found in the input images."""
-    pass
 
 
 class SiftResultCheckError(BaseError):
     """Exception raised for errors 0 sift points found in the input images."""
-    pass
 
 
 class HomographyError(BaseError):
     """In homography, find no mask, should kill points which is duplicate."""
-    pass
+
+
+class NoMatchPointError(BaseError):
+    """Exception raised for errors 0 keypoint found in the input images."""
+
+
+class MatchResultCheckError(BaseError):
+    """Exception raised for errors 0 keypoint found in the input images."""
