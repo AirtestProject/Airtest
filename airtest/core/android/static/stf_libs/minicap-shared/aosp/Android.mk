affe24d55ec30e54a5f8ea6ff6816f8f61485f0e
刘洪歧 <liuhongqi@corp.netease.com>
2019-11-01 16:20:59 +0800

    minicap merge pull190
    (cherry picked from commit 3970f56cb35fcab9ab65e47b84d7ee28c2baf5f6)
    (cherry picked from commit a3a6a191a2e541998bf691836cde45379bfe665e)

--- airtest/core/android/static/stf_libs/minicap-shared/aosp/Android.mk
+++ airtest/core/android/static/stf_libs/minicap-shared/aosp/Android.mk
@@ -7,6 +7,8 @@ LOCAL_MODULE_TAGS := optional
 
 ifneq ($(OVERRIDE_PLATFORM_SDK_VERSION),)
 LOCAL_SRC_FILES += src/minicap_$(OVERRIDE_PLATFORM_SDK_VERSION).cpp
+else ifeq ($(PLATFORM_SDK_VERSION),29)
+LOCAL_SRC_FILES += src/minicap_29.cpp
 else ifeq ($(PLATFORM_SDK_VERSION),28)
 LOCAL_SRC_FILES += src/minicap_28.cpp
 else ifeq ($(PLATFORM_SDK_VERSION),27)
