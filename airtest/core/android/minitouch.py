644bc81dfc7e7ffb3ee663c51493c7cb3a2c77bd
Tony <nitao.227@gmail.com>
2019-12-17 18:57:18 +0800

    Update minitouch.py
    还原之前的版本

--- airtest/core/android/minitouch.py
+++ airtest/core/android/minitouch.py
@@ -113,10 +113,7 @@ class Minitouch(object):
             transformed coordinates (x, y)
 
         """
-        if self.size_info["orientation"] == 1:
-            height, width = self.size_info['width'], self.size_info['height']
-        else:
-            width, height = self.size_info['width'], self.size_info['height']
+        width, height = self.size_info['width'], self.size_info['height']
 
         nx = x * self.max_x / width
         ny = y * self.max_y / height
