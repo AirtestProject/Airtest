9faa00f663dd1ee1e79969035531c856b01c83b3
王建军 <wjjn3033@corp.netease.com>
2020-01-11 21:14:18 +0800

    add: imread mode param.
    (cherry picked from commit bc19a5f47a04e539fbbe2760a2d0f0798fa3eb99)

--- airtest/aircv/aircv.py
+++ airtest/aircv/aircv.py
@@ -9,15 +9,20 @@ from six import PY2, PY3
 from airtest.aircv.utils import cv2_2_pil, compress_image
 
 
-def imread(filename):
+def imread(filename, flatten=False):
     """根据图片路径，将图片读取为cv2的图片处理格式."""
     if not os.path.isfile(filename):
         raise FileNotExistError("File not exist: %s" % filename)
+
+    # choose image readin mode: cv2.IMREAD_UNCHANGED=-1, cv2.IMREAD_GRAYSCALE=0, cv2.IMREAD_COLOR=1,
+    readin_mode = cv2.IMREAD_GRAYSCALE if flatten else cv2.IMREAD_COLOR
+
     if PY3:
-        img = cv2.imdecode(np.fromfile(filename, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+        img = cv2.imdecode(np.fromfile(filename, dtype=np.uint8), readin_mode)
     else:
         filename = filename.encode(sys.getfilesystemencoding())
-        img = cv2.imread(filename, 1)
+        img = cv2.imread(filename, readin_mode)
+
     return img
 
 
