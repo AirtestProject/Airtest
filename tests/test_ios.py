cbcea53c9cd40f636e45eee209fb80a7c2d071bf
张琦 <zhangqi2011@corp.netease.com>
2021-01-26 15:53:55 +0800

    Modify the ios unit test case
    (cherry picked from commit 0660cc3aa437b701ac9ea8c99386946d2778bc36)

--- tests/test_ios.py
+++ tests/test_ios.py
@@ -4,23 +4,22 @@ import time
 import unittest
 import numpy
 from airtest.core.ios.ios import IOS, wda
-from airtest.core.ios.constant import CAP_METHOD, TOUCH_METHOD, IME_METHOD
-from airtest.core.error import AirtestError
-from testconf import APK, PKG, try_remove
+from .testconf import try_remove
 
 text_flag = True # 控制是否运行text接口用例
-alert_flag = True # 控制是否测试alert相关接口用例
-addr="http://127.0.0.1:8100" # iOS设备连接参数
+skip_alert_flag = True  # 控制是否测试alert相关接口用例
+DEFAULT_ADDR = "http://localhost:8100/"  # iOS设备连接参数
+PKG_SAFARI = 'com.apple.mobilesafari'
+
 
 class TestIos(unittest.TestCase):
 
     @classmethod
-    def setUpClass(self):
-        self.ios = IOS(addr=addr)
-
+    def setUpClass(cls):
+        cls.ios = IOS(addr=DEFAULT_ADDR)
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         try_remove('screen.png')
 
     def test_session(self):
@@ -34,9 +33,9 @@ class TestIos(unittest.TestCase):
     def test_display_info(self):
         print("test_display_info")
         device_infos = self.ios.display_info
-        self.assertIn("width", device_infos)
-        self.assertIn("height", device_infos)
-        self.assertIn("orientation", device_infos)
+        self.assertIsInstance(device_infos["width"], int)
+        self.assertIsInstance(device_infos["height"], int)
+        self.assertIsInstance(device_infos["orientation"], str)
         print(device_infos)
 
     def test_snapshot(self):
@@ -53,6 +52,9 @@ class TestIos(unittest.TestCase):
     def test_keyevent_home(self):
         print("test_keyevent_home")
         self.ios.keyevent("home")
+
+        with self.assertRaises(ValueError):
+            self.ios.keyevent("home1")
     
     def test_keyevent_volume_up(self):
         print("test_keyevent_volume_up")
@@ -62,41 +64,50 @@ class TestIos(unittest.TestCase):
         print("test_keyevent_volume_down")
         self.ios.keyevent("voluMeDown")
 
+    def test_press(self):
+        with self.assertRaises(ValueError):
+            self.ios.press("home1")
+
+        self.ios.press("home")
+        self.ios.press("volumeUp")
+        self.ios.press("volumeDown")
 
     def test_home(self):
         print("test_home")
         self.ios.home()
+        self.assertTrue(self.ios.home_interface())
 
-    @unittest.skipIf(text_flag, "demonstrating skipping test_text")
     def test_text(self):
+        self.ios.start_app("com.apple.mobilenotes")
+        self.ios.touch((0.5, 0.5))
         self.ios.text('test text')
+        self.ios.text('中文')
+        self.ios.stop_app("com.apple.mobilenotes")
 
     def test_touch(self):
         # 位置参数可为：相对于设备的百分比坐标或者实际的逻辑位置坐标
         print("test_touch")
-        self.ios.touch((100, 100))
+        self.ios.touch((478, 360), duration=1)
+        self.ios.home()
+        time.sleep(2)
+        self.ios.touch((0.3, 0.3))
 
     def test_swipe(self):
-        # 位置参数只能为：相对于设备的百分比坐标
         print("test_swipe")
-        self.ios.swipe((0.1, 0.1), (0.3, 0.3))
+        # 右往左滑
+        self.ios.swipe((1050, 1900), (150, 1900))
+        time.sleep(2)
+        # 左往右滑
+        self.ios.swipe((0.2, 0.5), (0.8, 0.5))
 
-
-    def test_startapp(self):
-        print("test_startapp")
-        self.ios.start_app('com.apple.mobilesafari')
-
-    def test_stopapp(self):
-        print("test_stopapp")
-        self.ios.stop_app('com.apple.mobilesafari')
-    
     def test_lock(self):
         print("test_lock")
         self.ios.lock()
 
     def test_is_locked(self):
         print("test_is_locked")
-        print(self.ios.is_locked())
+        self.ios.unlock()
+        self.assertTrue(not self.ios.is_locked())
 
     def test_unlock(self):
         print("test_unlock")
@@ -108,48 +119,76 @@ class TestIos(unittest.TestCase):
 
     def test_double_click(self):
         print("test_double_click")
-        self.ios.double_click(0.5, 0.5)
+        self.ios.double_click((0.5, 0.5))
+        time.sleep(1)
+        self.ios.double_click((100, 100))
+
+    def test_startapp(self):
+        print("test_startapp")
+        self.ios.start_app(PKG_SAFARI)
+
+    def test_stopapp(self):
+        print("test_stopapp")
+        self.ios.stop_app(PKG_SAFARI)
 
     def test_app_state(self):
         print("test_app_state")
-        print(self.ios.app_state('com.apple.mobilesafari'))
+        self.ios.start_app(PKG_SAFARI)
+        print(self.ios.app_state(PKG_SAFARI))
+        self.assertEqual(self.ios.app_state(PKG_SAFARI)["value"], 4)
+        self.ios.home()
+        time.sleep(1)
+        print(self.ios.app_state(PKG_SAFARI))
+        self.assertEqual(self.ios.app_state(PKG_SAFARI)["value"], 3)
+        self.ios.stop_app(PKG_SAFARI)
+        time.sleep(1)
+        print(self.ios.app_state(PKG_SAFARI))
+        self.assertEqual(self.ios.app_state(PKG_SAFARI)["value"], 1)
 
     def test_app_current(self):
         print("test_app_current")
-        print(self.ios.app_current())
+        self.ios.start_app(PKG_SAFARI)
+        time.sleep(2)
+        self.assertEqual(self.ios.app_current()["bundleId"], PKG_SAFARI)
+        self.ios.stop_app(PKG_SAFARI)
     
     def test_get_ip_address(self):
         print("test_get_ip_address")
         print(self.ios.get_ip_address())
 
-    def get_device_status(self):
+    def test_device_status(self):
         print("test_get_device_status")
-        print(self.ios.device_status())
+        self.assertIsInstance(self.ios.device_status(), dict)
 
-    @unittest.skipIf(alert_flag, "demonstrating skipping get_alert_exists")
-    def get_alert_exists(self):
+    @unittest.skipIf(skip_alert_flag, "demonstrating skipping get_alert_exists")
+    def test_alert_exists(self):
+        # alert测试方法：手机开飞行模式断网，打开天气app，会弹出提示需要关闭飞行模式
+        # 重复测试时，需要先关闭app-关闭飞行模式-开启app-关闭app，然后重新开飞行模式
+        # 未安装手机sim卡时，在取消飞行模式的时候也会弹出一个“未安装SIM卡”的提示
         print("test_get_alert_exists")
         print(self.ios.alert_exists())
 
-    @unittest.skipIf(alert_flag, "demonstrating skipping get_alert_accept")
-    def get_alert_accept(self):
+    @unittest.skipIf(skip_alert_flag, "demonstrating skipping get_alert_accept")
+    def test_alert_accept(self):
         print("test_get_alert_accept")
-        self.ios.alert_accept()
+        self.assertTrue(self.ios.alert_exists())
+        print(self.ios.alert_accept())
 
-    @unittest.skipIf(alert_flag, "demonstrating skipping get_alert_dismiss")
-    def get_alert_dismiss(self):
+    @unittest.skipIf(skip_alert_flag, "demonstrating skipping get_alert_dismiss")
+    def test_alert_dismiss(self):
         print("test_get_alert_dismiss")
         self.ios.alert_dismiss()
 
-    @unittest.skipIf(alert_flag, "demonstrating skipping get_alert_buttons")
-    def get_alert_buttons(self):
+    @unittest.skipIf(skip_alert_flag, "demonstrating skipping get_alert_buttons")
+    def test_alert_buttons(self):
         print("test_get_alert_buttons")
         print(self.ios.alert_buttons())
+        print(self.ios.driver.alert.text)
 
-    @unittest.skipIf(alert_flag, "demonstrating skipping get_alert_click")
-    def get_alert_click(self):
+    @unittest.skipIf(skip_alert_flag, "demonstrating skipping get_alert_click")
+    def test_alert_click(self):
         print("test_get_alert_click")
-        self.ios.alert_click(['允许', '好'])
+        self.ios.alert_click(['设置', '允许', '好'])
 
     def test_device_info(self):
         print("test_device_info")
@@ -181,6 +220,7 @@ if __name__ == '__main__':
     suite.addTest(TestIos("test_home_interface"))
     suite.addTest(TestIos("test_touch"))
     suite.addTest(TestIos("test_swipe"))
+    suite.addTest(TestIos("test_double_click"))
     # 联合接口，顺序测试：解锁屏、应用启动关闭
     suite.addTest(TestIos("test_is_locked"))
     suite.addTest(TestIos("test_lock"))
