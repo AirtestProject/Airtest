04cc5665517f21d4a4b2aebacc5ff301d2aad59e
张琦 <zhangqi2011@corp.netease.com>
2021-02-08 10:48:35 +0800

    When import ios, use if to judge whether it is py2, remove try catch
    (cherry picked from commit c605f1cc85a48f98f5aa7b65abdbfe375d1de85b)

--- airtest/core/ios/__init__.py
+++ airtest/core/ios/__init__.py
@@ -1,4 +1,8 @@
 """
 This package provide IOS Device Class.
 """
+import six
+if six.PY2:
+    raise ImportError(
+        "The iOS module of Airtest>1.1.7 only supports Python3, if you want to use, please upgrade to Python3 first.")
 from airtest.core.ios.ios import IOS
