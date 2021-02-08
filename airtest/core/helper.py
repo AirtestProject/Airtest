c605f1cc85a48f98f5aa7b65abdbfe375d1de85b
张琦 <zhangqi2011@corp.netease.com>
2021-02-08 10:48:35 +0800

    When import ios, use if to judge whether it is py2, remove try catch

--- airtest/core/helper.py
+++ airtest/core/helper.py
@@ -162,11 +162,7 @@ def import_device_cls(platform):
     elif platform == "windows":
         from airtest.core.win.win import Windows as cls
     elif platform == "ios":
-        try:
-            # only support py3
-            from airtest.core.ios import IOS as cls
-        except:
-            raise ImportError("The iOS module of Airtest>1.1.7 only supports python3, if you want to use, please upgrade to python3 first.")
+        from airtest.core.ios import IOS as cls
     elif platform == "linux":
         from airtest.core.linux.linux import Linux as cls
     else:
