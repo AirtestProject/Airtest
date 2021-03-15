ec8858a01a5a5944c2bddd6608987ec244d89e03
胡骏飞 <you@example.com>
2021-03-15 15:34:42 +0800

    use hsv to cal confidence
    (cherry picked from commit 5241f79c19d428f7e33c502f744c1dc336960825)

--- airtest/aircv/cal_confidence.py
+++ airtest/aircv/cal_confidence.py
@@ -24,13 +24,15 @@ def cal_rgb_confidence(img_src_rgb, img_sch_rgb):
     """同大小彩图计算相似度."""
     # 扩展置信度计算区域
     img_sch_rgb = cv2.copyMakeBorder(img_sch_rgb, 10,10,10,10,cv2.BORDER_REPLICATE)
-    
+    # 转HSV强化颜色的影响
+    img_src_rgb = cv2.cvtColor(img_src_rgb, cv2.COLOR_BGR2HSV)
+    img_sch_rgb = cv2.cvtColor(img_sch_rgb, cv2.COLOR_BGR2HSV)
     src_bgr, sch_bgr = cv2.split(img_src_rgb), cv2.split(img_sch_rgb)
 
     # 计算BGR三通道的confidence，存入bgr_confidence:
     bgr_confidence = [0, 0, 0]
     for i in range(3):
-        res_temp = cv2.matchTemplate(src_bgr[i], sch_bgr[i], cv2.TM_CCORR_NORMED)
+        res_temp = cv2.matchTemplate(src_bgr[i], sch_bgr[i], cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res_temp)
         bgr_confidence[i] = max_val
 
