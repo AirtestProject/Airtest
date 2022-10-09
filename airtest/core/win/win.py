8e7c1749916d876e450fa4725f3d15ac013def83
张琦 <zhangqi2011@corp.netease.com>
2022-10-09 19:18:46 +0800

    change the format of win.screenshot to cv2
    (cherry picked from commit 0705171f5c16a700588e7fb41a2dc93180c4ba4e)

--- airtest/core/win/win.py
+++ airtest/core/win/win.py
@@ -121,7 +121,7 @@ class Windows(Device):
             monitor = self.screen.monitors[0]
         with mss.mss() as sct:
             sct_img = sct.grab(monitor)
-            screen = numpy.array(sct_img)
+            screen = aircv.utils.pil_2_cv2(sct_img)
             if filename:
                 aircv.imwrite(filename, screen, quality, max_size=max_size)
             return screen
