e1df937e4e465e43026d3e86f4718ce86d7676ab
张琦 <zhangqi2011@corp.netease.com>
2023-02-21 10:52:50 +0800

    if m1 mac, numpy >= 1.22
    (cherry picked from commit a7dcc88a61c5b46417b366de2fdf13ae2cafa8a4)

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import codecs
+import platform
 import pkg_resources
 from setuptools import setup, find_packages
 
@@ -36,6 +37,10 @@ def parse_requirements(filename):
         reqs.remove("facebook-wda>=1.3.3")
         reqs.remove("mss==6.1.0")
         reqs.append("mss==4.0.3")
+    # m1 mac, python>3.6
+    if platform.processor() == "arm":
+        reqs.remove("numpy<=1.19.5")
+        reqs.append("numpy")
     return reqs
 
 
