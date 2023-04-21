352858fa66f081a7059416b72c89d0bb2df042fc
张琦 <zhangqi2011@corp.netease.com>
2023-04-21 11:34:20 +0800

    add: add windows.set_focus api and support right click in swipe
    (cherry picked from commit 6a9bead85e3b3b21ea64d973f28c4766d5ecc6f3)

--- airtest/core/win/win.py
+++ airtest/core/win/win.py
@@ -13,7 +13,6 @@ import os
 from pywinauto.application import Application
 from pywinauto import mouse, keyboard
 from pywinauto.win32structures import RECT
-from pywinauto.win32functions import SetForegroundWindow
 
 from airtest.core.win.ctypesinput import key_press, key_release
 
@@ -276,7 +275,7 @@ class Windows(Device):
         coords = self._action_pos(pos)
         self.mouse.double_click(coords=coords)
 
-    def swipe(self, p1, p2, duration=0.8, steps=5):
+    def swipe(self, p1, p2, duration=0.8, steps=5, button="left"):
         """
         Perform swipe (mouse press and mouse release)
 
@@ -285,6 +284,7 @@ class Windows(Device):
             p2: end point
             duration: time interval to perform the swipe action
             steps: size of the swipe step
+            button: mouse button to press, 'left', 'right' or 'middle', default is 'left'
 
         Returns:
             None
@@ -298,7 +298,7 @@ class Windows(Device):
         to_x, to_y = self._action_pos(p2)
 
         interval = float(duration) / (steps + 1)
-        self.mouse.press(coords=(from_x, from_y))
+        self.mouse.press(coords=(from_x, from_y), button=button)
         time.sleep(interval)
         for i in range(1, steps):
             self.mouse.move(coords=(
@@ -309,7 +309,7 @@ class Windows(Device):
         for i in range(10):
             self.mouse.move(coords=(to_x, to_y))
         time.sleep(interval)
-        self.mouse.release(coords=(to_x, to_y))
+        self.mouse.release(coords=(to_x, to_y), button=button)
 
     def mouse_move(self, pos):
         """Simulates a `mousemove` event.
@@ -394,7 +394,9 @@ class Windows(Device):
             None
 
         """
-        SetForegroundWindow(self._top_window)
+        self._top_window.set_focus()
+
+    set_focus = set_foreground
 
     def get_rect(self):
         """
