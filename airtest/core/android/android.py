adc6a296ba64f1e5dc02f37826e9ce41ff12954a
zhangqi <zhangqi2011@corp.netease.com>
2025-04-08 09:58:41 +0800

    fix: get_frame maybe None
    (cherry picked from commit d4480e3c96cbce3fa8898419cf3b1b4f0c2a4a5d)

--- airtest/core/android/android.py
+++ airtest/core/android/android.py
@@ -967,6 +967,9 @@ class Android(Device):
         max_size = get_max_size(max_size)
         def get_frame():
             data = self.screen_proxy.get_frame_from_stream()
+            if data is None:
+                LOGGING.warning("get_frame_from_stream failed")
+                return None
             frame = aircv.utils.string_2_img(data)
 
             if max_size is not None:
